7444adc3a417608de683d2354ac6fe0b118c2620
Jacky <hklee310@gmail.com>
2016-10-18 03:42:33 -0700

    More data and working filters

--- app/scripts/controllers/googlemap.js
+++ app/scripts/controllers/googlemap.js
@@ -9,56 +9,16 @@
  */
 angular.module('angularApp')
   .controller('GooglemapCtrl', function ($scope, $http, $window) {
+    // Members
+    var map;
+    var infowindow;
 
-
-    // $scope.locations = [
-    //   {
-    //     "name":"Microsoft",
-    //     "city": "Redmond",
-    //     "costOfLiving": "High",
-    //     "lat": 47.65885,
-    //     "long": -122.14178
-    //   },
-    //   {
-    //     "name":'Facebook',
-    //     "city": "Menlo Park",
-    //     "costOfLiving": "Outragous",
-    //     "lat":37.48462,
-    //     "long":-122.14786
-    //   },
-    //   {
-    //     "name":'Zillow',
-    //     "city": "Seattle",
-    //     "costOfLiving": "High",
-    //     "lat": 47.60764,
-    //     "long": -122.33812
-    //   },
-    //   {
-    //     "name":'Epic',
-    //     "city": "Wisconsin",
-    //     "costOfLiving": "Dirt cheap",
-    //     "lat": 42.99682019999999,
-    //     "long": -89.56801139999999
-    //   },
-    //   // ['Amazon', 47.62229, -122.33649],
-    //   // ['Google', 37.42233, -122.08442],
-    //   // ['Plantir', 37.44260, -122.16216],
-    // ];
-
+    // Data access
     $http.get('data/companies.json').success(function(data) {
       $scope.locations = data.companies;
     });
 
-    $scope.orderByMe = function(x) {
-      $scope.myOrderBy = x;
-    }
-
-    function getRandomArbitrary(min, max) {
-      return Math.random() * (max - min) + min;
-    }
-
-    // $window.map;
-    var map;
+    // Init google map with markers
     $window.initMap = function(){
       map = new google.maps.Map(document.getElementById('map'), {
         zoom: 4,
@@ -66,8 +26,8 @@ angular.module('angularApp')
         mapTypeId: google.maps.MapTypeId.ROADMAP
       });
 
-      var infowindow = new google.maps.InfoWindow();
-
+      // Draw markers with mouseover display
+      infowindow = new google.maps.InfoWindow();
       var marker, i;
 
       for (i = 0; i < $scope.locations.length; i++) {
@@ -76,6 +36,7 @@ angular.module('angularApp')
           map: map
         });
 
+        // On Mouseover display infowindow over markers
         google.maps.event.addListener(marker, 'mouseover', (function(marker, i) {
           return function() {
             infowindow.setContent($scope.locations[i].name);
@@ -85,13 +46,38 @@ angular.module('angularApp')
       }
     }   // end initmap
 
+    // Pan to company location
     $scope.zoomInOnMap = function(lat, long){
-      // alert("To-Do: Zoom on map at " + lat + " " + long);
       var latLng = new google.maps.LatLng(lat,long);
       map.panTo(latLng);
       map.setZoom(6);
     }
 
 
+    // Filters
+    $scope.propertyName = 'name';
+    $scope.reverse = false;
+
+    $scope.sortBy = function(propertyName) {
+      $scope.reverse = ($scope.propertyName === propertyName) ? !$scope.reverse : false;
+      $scope.propertyName = propertyName;
+    };
+
+
+
+    $scope.orderByMe = function(x) {
+      $scope.myOrderBy = x;
+    }
+
+
+
+
+    $scope.searchString = {};
+    $scope.searchString.costOfLiving = '';
+
+
+    function getRandomArbitrary(min, max) {
+      return Math.random() * (max - min) + min;
+    }
 
   });
